ae4400e1006944b9a02706f0cd04db5d7d0fe03d
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-08-18 16:28:17 -0300

    atualizacao js

--- js/java.js
+++ js/java.js
@@ -5,3 +5,12 @@ menu.addEventListener('click',() => {
     menu.classList.toggle('ativo');
     NavMenu.classList.toggle('ativo');
 })
+
+//COMPORTAMENTO DA IMAGEM DO HOME
+document.addEventListener ( 'mousemove' , (e) => {
+    const x = (window.innerWidth / 2 - e.clientX) / 20;
+    const y = (window.innerHeight / 2 - e.clientY) / 20;
+
+    const img = document.querySelector('.home-img');
+    img.style.transform = `translate(${x}px, ${y}px`;
+});
